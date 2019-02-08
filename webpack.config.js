df4f27eeba575c391c3baa9f32516e0d0c270f6e
LAKSHITgoel <lakshit.goel@kuliza.com>
2019-02-08 14:19:35 +0530

    init

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: { main: "./src/index.js" },
+  output: {
+    filename: "bundle.js", //tyt4534gy34
+    path: __dirname + "/dist",
+    publicPath: "/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          { loader: "sass-loader" }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: "./dist",
+    hot: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HTMLWebpackPlugin({
+      template: "./dist/index.html"
+    })
+  ]
+};
+
+//splitchunks Plugin
